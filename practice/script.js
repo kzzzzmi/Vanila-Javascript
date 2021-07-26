ffa53d8839328ae751df647eabad41de952a5af1
kzzzzmi <jaemin21685@gmail.com>
2021-07-26 20:56:43 +0900

    studied chpater2

--- practice/script.js
+++ practice/script.js
@@ -1,1 +1,17 @@
-alert("자바스크립트!");
+
+let x = +prompt("x");
+let y = +prompt("y");
+
+while (y < 1) {
+  y = +prompt("y는 1보다 크거나 같아야 합니다.");
+}
+
+alert(pow(x, y));
+function pow(x, n) {
+  let answer = x;
+
+  for (let i = 1; i < n; i++) {
+    answer *= x;
+  }
+  return answer;
+}
